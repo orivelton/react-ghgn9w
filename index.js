5a203f340e8aa4801f99beccba5335d08a0b8e3f
orivelton <orivelton10@gmail.com>
2019-11-27 17:13:37 +0000

    Chore: removing a function

--- index.js
+++ index.js
@@ -7,15 +7,11 @@ class App extends Component {
   state = { count: 0 }
 
   increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
+   
   }
 
   decrement = () => {
-    this.setState({
-      count: this.state.count - 1
-    });
+    
   }
 
   render() {
